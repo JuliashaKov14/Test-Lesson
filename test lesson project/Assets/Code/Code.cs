685b76419532d0170f5857488d876cce6a586c6c
JuliashaKov14 <julia141407@gmail.com>
2017-03-21 23:24:21 +0200

    first commit

---
+++ test lesson project/Assets/Code/Code.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float Speed;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        Speed = 15f;
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(Horizontal, 0f, Vertical);
+        rb.AddForce(direction * Speed);	
+	}
+}
